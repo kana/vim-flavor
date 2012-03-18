5349bc6ab038222ee6e0f9b9988212da1af192b3
Kana Natsuno <dev@whileimautomaton.net>
2012-03-18 20:54:41 +0900

    Add CLI#install

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 require 'vim-flavor'
 
 describe Vim::Flavor::CLI do
+  describe '#install' do
+    it 'should use the default vimfiles path', :pending => true do end
+    it 'should use a given vimfiles path', :pending => true do end
+  end
 end
