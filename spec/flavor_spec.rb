dd8ec96e7d5182c18a74f0a6915934c9455f4462
Kana Natsuno <dev@whileimautomaton.net>
2012-03-18 18:40:42 +0900

    Refactor - Add spec/spec_helper.rb

--- spec/flavor_spec.rb
+++ spec/flavor_spec.rb
@@ -1,43 +1,9 @@
 require 'bundler/setup'
 require 'fileutils'
+require 'spec_helper'
 require 'vim-flavor'
 
 describe Vim::Flavor::Flavor do
-  def create_a_test_repo(path)
-    system(<<-"END")
-      {
-        mkdir -p #{path.inspect}
-        cd #{path.inspect}
-        git init
-        mkdir autoload doc plugin
-        touch autoload/foo.vim doc/foo.txt plugin/foo.vim
-        git add autoload doc plugin
-        git commit -am 'Commit foo'
-        echo '*foo*' >doc/foo.txt
-        git commit -am 'Update foo'
-        git tag -a -m 'Version 1.0.0' 1.0.0
-        git tag -a -m 'Version 1.1.1' 1.1.1
-        git tag -a -m 'Version 1.1.2' 1.1.2
-        git tag -a -m 'Version 1.2.1' 1.2.1
-        git tag -a -m 'Version 1.2.2' 1.2.2
-      } >/dev/null
-    END
-  end
-
-  def update_a_test_repo(path)
-    system(<<-"END")
-      {
-        cd #{path.inspect} &&
-        echo '*foo* *bar*' >doc/foo.txt &&
-        git commit -am 'Update foo again'
-      } >/dev/null
-    END
-  end
-
-  def clean_up_stashed_stuffs()
-    FileUtils.rm_rf([Vim::Flavor::DOT_PATH], :secure => true)
-  end
-
   describe '#clone' do
     before :each do
       @test_repo_path = "#{Vim::Flavor::DOT_PATH}/test/origin"
