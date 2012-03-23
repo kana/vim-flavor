0a4d3684cb3322e0d909c40d50216a7219ab1731
Kana Natsuno <dev@whileimautomaton.net>
2012-03-24 03:52:08 +0900

    Add Facade#create_vim_script_for_bootstrap

--- lib/vim-flavor.rb
+++ lib/vim-flavor.rb
@@ -1,4 +1,5 @@
 require 'bundler/setup'
+require 'fileutils'
 require 'thor'
 require 'vim-flavor/version'
 require 'yaml'
@@ -339,6 +340,40 @@ module Vim
         end
 
         new_flavors
+      end
+
+      def create_vim_script_for_bootstrap(vimfiles_path)
+        bootstrap_path = "#{vimfiles_path.to_flavors_path()}/bootstrap.vim"
+        FileUtils.mkdir_p(File.dirname(bootstrap_path))
+        File.open(bootstrap_path, 'w') do |f|
+          f.write(<<-'END')
+            function! s:bootstrap()
+              let current_rtp = &runtimepath
+              let current_rtps = split(current_rtp, ',')
+              set runtimepath&
+              let default_rtp = &runtimepath
+              let default_rtps = split(default_rtp, ',')
+              let user_dir = default_rtps[0]
+              let user_after_dir = default_rtps[-1]
+              let base_rtps =
+              \ filter(copy(current_rtps),
+              \        'v:val !=# user_dir && v:val !=# user_after_dir')
+              let flavor_dirs =
+              \ filter(split(glob(user_dir . '/flavors/*'), '\n'),
+              \        'isdirectory(v:val)')
+              let new_rtps =
+              \ []
+              \ + [user_dir]
+              \ + flavor_dirs
+              \ + base_rtps
+              \ + map(reverse(copy(flavor_dirs)), 'v:val . "/after"')
+              \ + [user_after_dir]
+              let &runtimepath = join(new_rtps, ',')
+            endfunction
+
+            call s:bootstrap()
+          END
+        end
       end
 
       def deploy_flavors(flavor_list, vimfiles_path)
