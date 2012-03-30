cff846d82833f5ec7134df83b13cb7f35a843cb7
Kana Natsuno <dev@whileimautomaton.net>
2012-03-30 20:58:02 +0900

    Separate files for CLI

--- lib/vim-flavor.rb
+++ lib/vim-flavor.rb
@@ -1,6 +1,7 @@
 require 'bundler/setup'
 require 'fileutils'
 require 'thor'
+require 'vim-flavor/cli'
 require 'vim-flavor/facade'
 require 'vim-flavor/flavor'
 require 'vim-flavor/flavorfile'
@@ -21,30 +22,6 @@ module Vim
 
       def dot_path= path
         @@dot_path = path
-      end
-    end
-
-    class CLI < Thor
-      desc 'install', 'Install Vim plugins according to VimFlavor file.'
-      method_option :vimfiles_path,
-        :desc => 'A path to your vimfiles directory.'
-      def install()
-        facade = Facade.new()
-        facade.traced = true
-        facade.install(
-          options[:vimfiles_path] || facade.get_default_vimfiles_path()
-        )
-      end
-
-      desc 'upgrade', 'Upgrade Vim plugins according to VimFlavor file.'
-      method_option :vimfiles_path,
-        :desc => 'A path to your vimfiles directory.'
-      def upgrade()
-        facade = Facade.new()
-        facade.traced = true
-        facade.upgrade(
-          options[:vimfiles_path] || facade.get_default_vimfiles_path()
-        )
       end
     end
   end
