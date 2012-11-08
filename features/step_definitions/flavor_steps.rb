230b14e4085a043d6958f0398bde73a944bd84be
Kana Natsuno <dev@whileimautomaton.net>
2012-11-08 18:41:53 +0900

    Write dummy step definitions

---
+++ features/step_definitions/flavor_steps.rb
@@ -0,0 +1,26 @@
+Given /^a temporary directory called '(.+)'$/ do |name|
+end
+
+Given /^a home directory called '(.+)' in '(.+)'$/ do |name, virtual_path|
+end
+
+Given /^a repository '(.+)' with versions '(.+)'$/ do |basename, versions|
+end
+
+Given 'flavorfile' do |content|
+end
+
+Given 'lockfile' do |content|
+end
+
+When /^I run vim-flavor with '(.+)'$/ do |args|
+end
+
+Then 'I get lockfile' do |content|
+end
+
+Then /^I get a bootstrap script in '(.+)'$/ do |virtual_path|
+end
+
+Then /^I get flavor '(.+)' with '(.+)' in '(.+)'$/ do |basename, version, virtual_path|
+end
