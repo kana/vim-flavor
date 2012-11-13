38600a39b54ab8591454834c7dc255c8c901b1e4
Kana Natsuno <dev@whileimautomaton.net>
2012-11-13 18:18:38 +0900

    Add a skeleton of the "install" task

--- lib/vim-flavor/cli.rb
+++ lib/vim-flavor/cli.rb
@@ -3,6 +3,9 @@ require 'thor'
 module Vim
   module Flavor
     class CLI < Thor
+      desc 'install', 'Install Vim plugins according to VimFlavor file.'
+      def install
+      end
     end
   end
 end
