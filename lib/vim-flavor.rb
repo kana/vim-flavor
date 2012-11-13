a27a1f41ceb01ea66d9187501d7ecb78fc59897d
Kana Natsuno <dev@whileimautomaton.net>
2012-11-13 22:02:22 +0900

    Define StringExtension

--- lib/vim-flavor.rb
+++ lib/vim-flavor.rb
@@ -5,9 +5,14 @@ module Vim
     [
       :CLI,
       :Facade,
+      :StringExtension,
       :VERSION,
     ].each do |name|
       autoload name, "vim-flavor/#{name.to_s().downcase()}"
+    end
+
+    class ::String
+      include StringExtension
     end
   end
 end
---
+++ lib/vim-flavor/stringextension.rb
@@ -0,0 +1,25 @@
+module Vim
+  module Flavor
+    module StringExtension
+      def to_bootstrap_path
+        "#{self}/bootstrap.vim"
+      end
+
+      def to_flavorfile_path
+        "#{self}/VimFlavor"
+      end
+
+      def to_flavors_path
+        "#{self}/flavors"
+      end
+
+      def to_lockfile_path
+        "#{self}/VimFlavor.lock"
+      end
+
+      def to_vimfiles_path
+        "#{self}/.vim"
+      end
+    end
+  end
+end
