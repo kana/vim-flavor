7ba3efd26a741ca7875b02647bb4720f62a80dfb
Kana Natsuno <dev@whileimautomaton.net>
2012-11-13 18:18:20 +0900

    Add a skeleton of Vim::Flavor::CLI

--- lib/vim-flavor.rb
+++ lib/vim-flavor.rb
@@ -3,6 +3,7 @@ require 'bundler/setup'
 module Vim
   module Flavor
     [
+      :CLI,
       :VERSION,
     ].each do |name|
       autoload name, "vim-flavor/#{name.to_s().downcase()}"
---
+++ lib/vim-flavor/cli.rb
@@ -0,0 +1,8 @@
+require 'thor'
+
+module Vim
+  module Flavor
+    class CLI < Thor
+    end
+  end
+end
