efc4eb8c784205d8fc507a790ba4800edbf43edc
Kana Natsuno <dev@whileimautomaton.net>
2012-11-15 01:43:14 +0900

    Define Flavor#version_constraint

--- lib/vim-flavor/flavor.rb
+++ lib/vim-flavor/flavor.rb
@@ -5,6 +5,9 @@ module Vim
       # Possible formats are "$user/$repo", "$repo" and "$repo_uri".
       attr_accessor :repo_name
 
+      # A constraint to choose a proper version.
+      attr_accessor :version_constraint
+
       # A version of a plugin to be installed.
       attr_accessor :locked_version
     end
