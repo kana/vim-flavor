76b960a88f5591ec225f1f5d800acae050c0c8c2
Kana Natsuno <dev@whileimautomaton.net>
2012-11-14 23:38:38 +0900

    Define Flavor#repo_name

--- lib/vim-flavor/flavor.rb
+++ lib/vim-flavor/flavor.rb
@@ -1,6 +1,9 @@
 module Vim
   module Flavor
     class Flavor
+      # A short name of a repository.
+      # Possible formats are "$user/$repo", "$repo" and "$repo_uri".
+      attr_accessor :repo_name
     end
   end
 end
