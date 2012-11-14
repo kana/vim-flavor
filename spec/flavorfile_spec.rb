d207b1272fd837fddf9a0e8a069f8f75fbc8631a
Kana Natsuno <dev@whileimautomaton.net>
2012-11-15 01:29:30 +0900

    Add spec for FlavorFile#flavor

--- spec/flavorfile_spec.rb
+++ spec/flavorfile_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 module Vim
   module Flavor
     describe FlavorFile do
+      describe '#flavor' do
+        it 'registers a flavor' do
+          ff = FlavorFile.new()
+          ff.flavor 'kana/vim-altr', '>= 1.2.3'
+          f = ff.flavor_table['kana/vim-altr']
+          f.repo_name.should == 'kana/vim-altr'
+          f.version_constraint.should == VersionConstraint.new('>= 1.2.3')
+        end
+
+        it 'completes version constraint if it is not given' do
+          ff = FlavorFile.new()
+          ff.flavor 'kana/vim-altr'
+          f = ff.flavor_table['kana/vim-altr']
+          f.repo_name.should == 'kana/vim-altr'
+          f.version_constraint.should == VersionConstraint.new('>= 0')
+        end
+      end
     end
   end
 end
