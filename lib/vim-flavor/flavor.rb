e03af7c929e72935bf99018d4ac4750ea2ff590c
Kana Natsuno <dev@whileimautomaton.net>
2012-11-14 23:38:46 +0900

    Define Flavor#locked_version

--- lib/vim-flavor/flavor.rb
+++ lib/vim-flavor/flavor.rb
@@ -4,6 +4,9 @@ module Vim
       # A short name of a repository.
       # Possible formats are "$user/$repo", "$repo" and "$repo_uri".
       attr_accessor :repo_name
+
+      # A version of a plugin to be installed.
+      attr_accessor :locked_version
     end
   end
 end
