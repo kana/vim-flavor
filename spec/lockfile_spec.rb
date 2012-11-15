e02165dcc766e66bfcd641aae4ed94f7da2538cf
Kana Natsuno <dev@whileimautomaton.net>
2012-11-16 01:59:43 +0900

    Add spec for LockFile#load

--- spec/lockfile_spec.rb
+++ spec/lockfile_spec.rb
@@ -36,6 +36,29 @@ module Vim
             ['foo (1.2.3)']
         end
       end
+
+      describe '#load' do
+        it 'loads locked information from a lockfile' do
+          File.open(@tmp_path.to_lockfile_path, 'w') do |io|
+            io.write(<<-'END')
+              foo (1.2.3)
+              bar (2.3.4)
+              baz (3.4.5)
+            END
+          end
+
+          l = LockFile.new(@tmp_path.to_lockfile_path)
+          l.flavor_table.should be_empty
+
+          l.load()
+          l.flavor_table['foo'].repo_name.should == 'foo'
+          l.flavor_table['foo'].locked_version.should == '1.2.3'
+          l.flavor_table['bar'].repo_name.should == 'bar'
+          l.flavor_table['bar'].locked_version.should == '2.3.4'
+          l.flavor_table['baz'].repo_name.should == 'baz'
+          l.flavor_table['baz'].locked_version.should == '3.4.5'
+        end
+      end
     end
   end
 end
