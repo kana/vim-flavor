7a30bae41416c202c0060653c78eb50ab23a15a5
Kana Natsuno <dev@whileimautomaton.net>
2012-11-15 18:06:36 +0900

    Define VersionConstraint properties

--- lib/vim-flavor/versionconstraint.rb
+++ lib/vim-flavor/versionconstraint.rb
@@ -1,6 +1,11 @@
 module Vim
   module Flavor
     class VersionConstraint
+      attr_reader :base_version
+
+      # Specifies how to choose a suitable version according to base_version.
+      attr_reader :qualifier
+
       def initialize(s)
         # FIXME
       end
