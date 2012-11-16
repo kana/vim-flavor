8f72d14c264c95c0311a08618939879eaad26104
Kana Natsuno <dev@whileimautomaton.net>
2012-11-17 01:57:10 +0900

    Use make_flavor_path

--- features/step_definitions/flavor_steps.rb
+++ features/step_definitions/flavor_steps.rb
@@ -84,13 +84,13 @@ end
 
 Then /^I get flavor '(.+)' with '(.+)' in '(.+)'$/ do |basename, version, virtual_path|
   repo_name = make_repo_uri(basename)
-  flavor_path = expand("#{virtual_path.to_flavors_path}/#{repo_name.zap}")
+  flavor_path = make_flavor_path(virtual_path, repo_name)
   File.open("#{flavor_path}/doc/#{basename}.txt", 'r').read().should ==
     "*#{basename}* #{version}\n"
 end
 
 Then /^I don't have flavor '(.+)' in '(.+)'$/ do |basename, virtual_path|
   repo_name = make_repo_uri(basename)
-  flavor_path = expand("#{virtual_path.to_flavors_path}/#{repo_name.zap}")
+  flavor_path = make_flavor_path(virtual_path, repo_name)
   Dir.should_not exist(flavor_path)
 end
