e3c4fd98f46486ebade43cd22bef0605b390673b
Kana Natsuno <dev@whileimautomaton.net>
2012-11-20 23:54:04 +0900

    Define EnumerableExtension#before_each

--- lib/vim-flavor/enumerableextension.rb
+++ lib/vim-flavor/enumerableextension.rb
@@ -3,6 +3,16 @@ module Vim
     EnumerableExtension = ::Enumerable
 
     module EnumerableExtension
+      def before_each(&block)
+        vs = each
+        Enumerator.new do |y|
+          loop do
+            v = vs.next
+            block.call(v)
+            y << v
+          end
+        end
+      end
     end
   end
 end
