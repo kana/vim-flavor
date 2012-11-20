5c67d0462326ca198f1eb5bc704de91fae9051ad
Kana Natsuno <dev@whileimautomaton.net>
2012-11-20 23:15:46 +0900

    Add a spec for EnumerableExtension#before_each

--- spec/enumerableextension_spec.rb
+++ spec/enumerableextension_spec.rb
@@ -3,6 +3,25 @@ require 'spec_helper'
 module Vim
   module Flavor
     describe EnumerableExtension do
+      describe '#before_each' do
+        it 'runs a given block before enumerating each element' do
+          xs = []
+          [1, 2, 3].
+          before_each {|n| xs << "before_each #{n}"}.
+          each do |n|
+            xs << "each #{n}"
+          end
+
+          xs.should == [
+            "before_each 1",
+            "each 1",
+            "before_each 2",
+            "each 2",
+            "before_each 3",
+            "each 3",
+          ]
+        end
+      end
     end
   end
 end
