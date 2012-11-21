5975874e41e8609c3830240df2e12b7e864f71f2
Kana Natsuno <dev@whileimautomaton.net>
2012-11-21 19:48:12 +0900

    Add a spec for EnumerableExtension#after_each

--- spec/enumerableextension_spec.rb
+++ spec/enumerableextension_spec.rb
@@ -22,6 +22,26 @@ module Vim
           ]
         end
       end
+
+      describe '#after_each' do
+        it 'runs a given block after enumerating each element' do
+          xs = []
+          [1, 2, 3].
+          after_each {|n| xs << "after_each #{n}"}.
+          each do |n|
+            xs << "each #{n}"
+          end
+
+          xs.should == [
+            "each 1",
+            "after_each 1",
+            "each 2",
+            "after_each 2",
+            "each 3",
+            "after_each 3",
+          ]
+        end
+      end
     end
   end
 end
