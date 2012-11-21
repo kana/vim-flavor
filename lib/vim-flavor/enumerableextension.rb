4a9f5a82f956ea31bcccca05b631acf7d4131938
Kana Natsuno <dev@whileimautomaton.net>
2012-11-21 19:49:05 +0900

    Define EnumerableExtension#after_each

--- lib/vim-flavor/enumerableextension.rb
+++ lib/vim-flavor/enumerableextension.rb
@@ -13,6 +13,17 @@ module Vim
           end
         end
       end
+
+      def after_each(&block)
+        vs = each
+        Enumerator.new do |y|
+          loop do
+            v = vs.next
+            y << v
+            block.call(v)
+          end
+        end
+      end
     end
   end
 end
