305148a30275a52cb8538a15ac475c5361c6e832
Kana Natsuno <dev@whileimautomaton.net>
2012-12-06 00:01:14 +0900

    Add a step to remove an arbitrary directory

--- features/step_definitions/directory_steps.rb
+++ features/step_definitions/directory_steps.rb
@@ -7,3 +7,10 @@ end
 Given /^I delete '(.+)'$/ do |virtual_path|
   delete_path expand(virtual_path)
 end
+
+When /^I remove the directory "([^"]*)"$/ do |dir_name|
+  in_current_dir do
+    # FileUtils#rmdir cannot delete non-empty directories.
+    FileUtils.rm_r(dir_name)
+  end
+end
