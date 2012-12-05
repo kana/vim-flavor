295a30884c1ed9a0f5215b2f76c716e39fbc5125
Kana Natsuno <dev@whileimautomaton.net>
2012-12-05 20:21:30 +0900

    Delete legacy steps about $tmp and $home

--- features/step_definitions/directory_steps.rb
+++ features/step_definitions/directory_steps.rb
@@ -1,19 +1,5 @@
 require 'tmpdir'
 
-Given /^a temporary directory called '(.+)'$/ do |name|
-  path = Dir.mktmpdir
-  at_exit do
-    delete_path path
-  end
-  variable_table[name] = path
-end
-
-Given /^a home directory called '(.+)' in '(.+)'$/ do |name, virtual_path|
-  actual_path = expand(virtual_path)
-  Dir.mkdir actual_path, 0700
-  variable_table[name] = actual_path
-end
-
 Given /^I don't have a directory called '(.+)'$/ do |virtual_path|
   Dir.should_not exist(expand(virtual_path))
 end
