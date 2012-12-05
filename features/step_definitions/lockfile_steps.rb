2d19a717739d30fbbbdba250f6a67d0c35f8875f
Kana Natsuno <dev@whileimautomaton.net>
2012-12-05 21:07:42 +0900

    Add more steps to verify a lockfile content

--- features/step_definitions/lockfile_steps.rb
+++ features/step_definitions/lockfile_steps.rb
@@ -11,7 +11,7 @@ Given /^I delete lockfile$/ do
   delete_path expand('$tmp').to_lockfile_path
 end
 
-Then 'a lockfile is created with:' do |content|
+Then /^(?:a|the) lockfile is (?:created|updated) with:$/ do |content|
   # For some reason, Cucumber drops the last newline from every docstring...
   steps %Q{
     Then the file "#{'.'.to_lockfile_path}" should contain exactly:
