ab8927213fba8b932637250a41ef64c36c522f40
Kana Natsuno <dev@whileimautomaton.net>
2012-12-05 20:10:09 +0900

    Rewrite a step to verify a bootstrap script

--- features/step_definitions/bootstrap_script_steps.rb
+++ features/step_definitions/bootstrap_script_steps.rb
@@ -1,7 +1,6 @@
-Then /^I get a bootstrap script in '(.+)'$/ do |virtual_path|
-  File.should exist(
-    expand(virtual_path).
-    to_flavors_path.
-    to_bootstrap_path
-  )
+Then /^a bootstrap script is created in "(.+)"$/ do |v_vimfiles_path|
+  p = expand(v_vimfiles_path).to_flavors_path.to_bootstrap_path
+  steps %Q{
+    Then a file named "#{p}" should exist
+  }
 end
