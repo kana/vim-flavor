ed8c55041f8833065b784b70072f9a5dffd8ec0c
Kana Natsuno <dev@whileimautomaton.net>
2012-12-05 19:50:44 +0900

    Rewrite a step to create a flavorfile

--- features/step_definitions/flavorfile_steps.rb
+++ features/step_definitions/flavorfile_steps.rb
@@ -1,5 +1,10 @@
-Given 'flavorfile' do |content|
-  create_file expand('$tmp').to_flavorfile_path, expand(content)
+Given 'a flavorfile with:' do |content|
+  steps %Q{
+    Given a file named "#{'.'.to_flavorfile_path}" with:
+      """
+      #{expand(content)}
+      """
+  }
 end
 
 When 'I edit flavorfile as' do |content|
