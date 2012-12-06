03ad6af1dea5b7a44dfa0467023871f8f99462a9
Kana Natsuno <dev@whileimautomaton.net>
2012-12-06 22:16:17 +0900

    Rewrite a step to create an executable file

--- features/step_definitions/file_steps.rb
+++ features/step_definitions/file_steps.rb
@@ -2,8 +2,12 @@ Given /^a file called '(.*)'$/ do |virtual_path, content|
   create_file expand(virtual_path), expand(content)
 end
 
-Given /^an executable called '(.*)'$/ do |virtual_path, content|
-  path = expand(virtual_path)
-  create_file path, expand(content)
-  File.chmod(0755, path)
+Given /^an executable file named "(.*)" with:$/ do |file_path, content|
+  steps %Q{
+    Given a file named "#{file_path}" with:
+      """
+      #{content}
+      """
+  }
+  File.chmod(0755, File.join([current_dir, file_path]))
 end
