701b018ed3f4ac7f2c0f99920d1d2d6e55546402
Kana Natsuno <dev@whileimautomaton.net>
2012-12-06 22:33:08 +0900

    Delete legacy steps for directories

--- features/step_definitions/directory_steps.rb
+++ features/step_definitions/directory_steps.rb
@@ -1,13 +1,3 @@
-require 'tmpdir'
-
-Given /^I don't have a directory called '(.+)'$/ do |virtual_path|
-  Dir.should_not exist(expand(virtual_path))
-end
-
-Given /^I delete '(.+)'$/ do |virtual_path|
-  delete_path expand(virtual_path)
-end
-
 When /^I remove the directory "([^"]*)"$/ do |dir_name|
   in_current_dir do
     # FileUtils#rmdir cannot delete non-empty directories.
