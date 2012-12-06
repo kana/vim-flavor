556a0aafc8e22f655f8b9818a3f80e2c92a2cd5c
Kana Natsuno <dev@whileimautomaton.net>
2012-12-06 22:33:52 +0900

    Delete a legacy step for files

--- features/step_definitions/file_steps.rb
+++ features/step_definitions/file_steps.rb
@@ -1,7 +1,3 @@
-Given /^a file called '(.*)'$/ do |virtual_path, content|
-  create_file expand(virtual_path), expand(content)
-end
-
 Given /^an executable file named "(.*)" with:$/ do |file_path, content|
   steps %Q{
     Given a file named "#{file_path}" with:
