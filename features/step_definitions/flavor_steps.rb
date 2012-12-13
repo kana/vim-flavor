b2b71a8330a31e226e8929df4a49fd1bf55800b3
Kana Natsuno <dev@whileimautomaton.net>
2012-12-13 18:29:58 +0900

    Reduce duplicate on steps to create new versions

--- features/step_definitions/flavor_steps.rb
+++ features/step_definitions/flavor_steps.rb
@@ -53,20 +53,9 @@ Given /^a (?:(?:GitHub|local) )?repository "(.+)" with versions "(.+)"$/ do |bas
 end
 
 Given /^"(.+)" version "(.+)" is released$/ do |basename, version|
-  repository_path = make_repo_path(basename)
-  doc_name = basename.split('/').last.sub(/^vim-/, '')
-  sh <<-"END"
-    {
-      cd '#{repository_path}' &&
-      for v in #{version}
-      do
-        echo "*#{doc_name}* $v" >'doc/#{doc_name}.txt'
-        git add doc
-        git commit -m "Version $v"
-        git tag -m "Version $v" "$v"
-      done
-    } >/dev/null
-  END
+  steps %Q{
+    Given the repository "#{basename}" has versions "#{version}"
+  }
 end
 
 Given /^a repository "(.+)" from offline cache$/ do |repo_name|
