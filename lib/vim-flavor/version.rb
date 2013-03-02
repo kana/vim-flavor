78dcd687738ebfa128aca23f9646c7cc2f5c9ed4
Kana Natsuno <dev@whileimautomaton.net>
2013-03-03 01:41:38 +0900

    Accept "vX.Y.Z" style tags as versions

--- lib/vim-flavor/version.rb
+++ lib/vim-flavor/version.rb
@@ -3,6 +3,24 @@ module Vim
     VERSION = '1.1.0'
 
     class Version < Gem::Version
+      attr_accessor :original_tag_name
+
+      def initialize(tag_name)
+        @original_tag_name = tag_name
+        super(self.class.normalize_tag_name(tag_name))
+      end
+
+      def self.normalize_tag_name(tag_name)
+        tag_name.sub(/^v/, '')
+      end
+
+      def self.correct?(tag_name)
+        super(normalize_tag_name(tag_name))
+      end
+
+      def to_s()
+        @original_tag_name
+      end
     end
   end
 end
