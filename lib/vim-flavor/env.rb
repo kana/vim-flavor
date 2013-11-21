16eb40e1bc4b9bdf942876570b2c089b345d5171
Kana Natsuno <dev@whileimautomaton.net>
2013-11-21 23:53:35 +0900

    Define Env::github_uri_prefix

--- lib/vim-flavor/env.rb
+++ lib/vim-flavor/env.rb
@@ -1,6 +1,13 @@
 module Vim
   module Flavor
     class Env
+      class << self
+        def github_uri_prefix
+          @github_uri_prefix ||=
+            ENV['VIM_FLAVOR_GITHUB_URI_PREFIX'] ||
+            'git://github.com/'
+        end
+      end
     end
   end
 end
