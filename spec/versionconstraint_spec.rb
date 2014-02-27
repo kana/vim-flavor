64b5c116ff84263c6ec5751a5acb9f4d57eb2ad7
Kana Natsuno <dev@whileimautomaton.net>
2014-02-27 20:55:53 +0900

    Use only Version instances to judge compatibility

--- spec/versionconstraint_spec.rb
+++ spec/versionconstraint_spec.rb
@@ -3,52 +3,57 @@ require 'spec_helper'
 module Vim
   module Flavor
     describe VersionConstraint do
+      def v(s)
+        Version.create(s)
+      end
+
       describe '#compatible?' do
         describe '>=' do
           subject {VersionConstraint.new('>= 1.2.3')}
 
-          it {should be_compatible '1.2.3'}
+          it {should be_compatible v('1.2.3')}
 
-          it {should be_compatible '1.2.4'}
-          it {should be_compatible '1.3.3'}
-          it {should be_compatible '2.2.3'}
+          it {should be_compatible v('1.2.4')}
+          it {should be_compatible v('1.3.3')}
+          it {should be_compatible v('2.2.3')}
 
-          it {should be_compatible '1.3'}
+          it {should be_compatible v('1.3')}
 
-          it {should_not be_compatible '1.2.2'}
-          it {should_not be_compatible '1.1.3'}
-          it {should_not be_compatible '0.2.3'}
+          it {should_not be_compatible v('1.2.2')}
+          it {should_not be_compatible v('1.1.3')}
+          it {should_not be_compatible v('0.2.3')}
 
-          it {should_not be_compatible '1.2'}
+          it {should_not be_compatible v('1.2')}
         end
 
         describe '~>' do
           subject {VersionConstraint.new('~> 1.2.3')}
 
-          it {should be_compatible '1.2.3'}
+          it {should be_compatible v('1.2.3')}
 
-          it {should be_compatible '1.2.4'}
-          it {should_not be_compatible '1.3.3'}
-          it {should_not be_compatible '2.2.3'}
+          it {should be_compatible v('1.2.4')}
+          it {should_not be_compatible v('1.3.3')}
+          it {should_not be_compatible v('2.2.3')}
 
-          it {should_not be_compatible '1.3'}
+          it {should_not be_compatible v('1.3')}
 
-          it {should_not be_compatible '1.2.2'}
-          it {should_not be_compatible '1.1.3'}
-          it {should_not be_compatible '0.2.3'}
+          it {should_not be_compatible v('1.2.2')}
+          it {should_not be_compatible v('1.1.3')}
+          it {should_not be_compatible v('0.2.3')}
 
-          it {should_not be_compatible '1.2'}
+          it {should_not be_compatible v('1.2')}
         end
       end
 
       describe '#find_the_best_version' do
         it 'returns the best version from given versions' do
+          versions = ['1.2.2', '1.2.3', '1.2.4', '1.3.3', '2.0'].map {|s| v(s)}
           VersionConstraint.new('>= 1.2.3').
-            find_the_best_version(['1.2.2', '1.2.3', '1.2.4', '1.3.3', '2.0']).
-            should == '2.0'
+            find_the_best_version(versions).
+            should == v('2.0')
           VersionConstraint.new('~> 1.2.3').
-            find_the_best_version(['1.2.2', '1.2.3', '1.2.4', '1.3.3', '2.0']).
-            should == '1.2.4'
+            find_the_best_version(versions).
+            should == v('1.2.4')
         end
 
         it 'fails if no version is given' do
