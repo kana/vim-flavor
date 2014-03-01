a5fa5bd9640c8d07c4be31136a0934280e1294bc
Kana Natsuno <dev@whileimautomaton.net>
2014-03-01 16:19:10 +0900

    Change Version as a proxy of PlainVersion

--- lib/vim-flavor/version.rb
+++ lib/vim-flavor/version.rb
@@ -2,24 +2,13 @@ module Vim
   module Flavor
     VERSION = '1.1.5'
 
-    class Version < Gem::Version
-      attr_accessor :original_tag_name
-
-      def initialize(tag_name)
-        @original_tag_name = tag_name
-        super(self.class.normalize_tag_name(tag_name))
+    class Version
+      def self.create(*args)
+        PlainVersion.create(*args)
       end
 
-      def self.normalize_tag_name(tag_name)
-        tag_name.to_s.sub(/^v/, '')
-      end
-
-      def self.correct?(tag_name)
-        super(normalize_tag_name(tag_name))
-      end
-
-      def to_s()
-        @original_tag_name
+      def self.correct?(*args)
+        PlainVersion.correct?(*args)
       end
     end
   end
--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -6,61 +6,21 @@ module Vim
       v = described_class
 
       describe '::create' do
-        it 'accept "X.Y.Z" style tags' do
-          expect {
-            v.create('1')
-            v.create('1.2')
-            v.create('1.2.3')
-          }.not_to raise_error
-        end
-
-        it 'accept "vX.Y.Z" style tags' do
-          expect {
-            v.create('v1')
-            v.create('v1.2')
-            v.create('v1.2.3')
-          }.not_to raise_error
+        it 'makes a PlainVersion' do
+          expect(v.create('1.2.3')).to be_a(PlainVersion)
+          expect(v.create('v1.2.3')).to be_a(PlainVersion)
         end
       end
 
       describe '::correct?' do
-        it 'accept "X.Y.Z" style tags' do
+        it 'is an alias of PlainVersion::correct?' do
           expect(v.correct?('1')).to be_true
           expect(v.correct?('1.2')).to be_true
           expect(v.correct?('1.2.3')).to be_true
-        end
-
-        it 'accept "vX.Y.Z" style tags' do
           expect(v.correct?('v1')).to be_true
           expect(v.correct?('v1.2')).to be_true
           expect(v.correct?('v1.2.3')).to be_true
-        end
-      end
-
-      describe '#<=>' do
-        it 'can comapre "X.Y.Z" tags' do
-          expect(v.create('1.2.3')).to be <= v.create('1.3')
-          expect(v.create('1.2.3')).to be == v.create('1.2.3')
-          expect(v.create('1.2.3')).to be >= v.create('1.1')
-        end
-
-        it 'can comapre "vX.Y.Z" tags' do
-          expect(v.create('v1.2.3')).to be <= v.create('v1.3')
-          expect(v.create('v1.2.3')).to be == v.create('v1.2.3')
-          expect(v.create('v1.2.3')).to be >= v.create('v1.1')
-        end
-
-        it 'can comapre "X.Y.Z" tag with "vX.Y.Z" tag' do
-          expect(v.create('1.2.3')).to be <= v.create('v1.3')
-          expect(v.create('1.2.3')).to be == v.create('v1.2.3')
-          expect(v.create('1.2.3')).to be >= v.create('v1.1')
-        end
-      end
-
-      describe '#to_s' do
-        it 'is converted into the original string' do
-          expect(v.create('1.2.3').to_s).to be == '1.2.3'
-          expect(v.create('v1.2.3').to_s).to be == 'v1.2.3'
+          expect(v.correct?('vim7.4')).to be_false
         end
       end
     end
