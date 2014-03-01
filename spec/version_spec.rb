3e192c5c757286c375bd467a8dfb6a7ddc8d8b93
Kana Natsuno <dev@whileimautomaton.net>
2014-03-01 21:52:05 +0900

    Avoid writing described_class directly

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,62 +3,64 @@ require 'spec_helper'
 module Vim
   module Flavor
     describe Version do
+      v = described_class
+
       describe '::create' do
         it 'accept "X.Y.Z" style tags' do
           expect {
-            Version.create('1')
-            Version.create('1.2')
-            Version.create('1.2.3')
+            v.create('1')
+            v.create('1.2')
+            v.create('1.2.3')
           }.not_to raise_error
         end
 
         it 'accept "vX.Y.Z" style tags' do
           expect {
-            Version.create('v1')
-            Version.create('v1.2')
-            Version.create('v1.2.3')
+            v.create('v1')
+            v.create('v1.2')
+            v.create('v1.2.3')
           }.not_to raise_error
         end
       end
 
       describe '::correct?' do
         it 'accept "X.Y.Z" style tags' do
-          expect(Version.correct?('1')).to be_true
-          expect(Version.correct?('1.2')).to be_true
-          expect(Version.correct?('1.2.3')).to be_true
+          expect(v.correct?('1')).to be_true
+          expect(v.correct?('1.2')).to be_true
+          expect(v.correct?('1.2.3')).to be_true
         end
 
         it 'accept "vX.Y.Z" style tags' do
-          expect(Version.correct?('v1')).to be_true
-          expect(Version.correct?('v1.2')).to be_true
-          expect(Version.correct?('v1.2.3')).to be_true
+          expect(v.correct?('v1')).to be_true
+          expect(v.correct?('v1.2')).to be_true
+          expect(v.correct?('v1.2.3')).to be_true
         end
       end
 
       describe '#<=>' do
         it 'can comapre "X.Y.Z" tags' do
-          expect(Version.create('1.2.3')).to be <= Version.create('1.3')
-          expect(Version.create('1.2.3')).to be == Version.create('1.2.3')
-          expect(Version.create('1.2.3')).to be >= Version.create('1.1')
+          expect(v.create('1.2.3')).to be <= v.create('1.3')
+          expect(v.create('1.2.3')).to be == v.create('1.2.3')
+          expect(v.create('1.2.3')).to be >= v.create('1.1')
         end
 
         it 'can comapre "vX.Y.Z" tags' do
-          expect(Version.create('v1.2.3')).to be <= Version.create('v1.3')
-          expect(Version.create('v1.2.3')).to be == Version.create('v1.2.3')
-          expect(Version.create('v1.2.3')).to be >= Version.create('v1.1')
+          expect(v.create('v1.2.3')).to be <= v.create('v1.3')
+          expect(v.create('v1.2.3')).to be == v.create('v1.2.3')
+          expect(v.create('v1.2.3')).to be >= v.create('v1.1')
         end
 
         it 'can comapre "X.Y.Z" tag with "vX.Y.Z" tag' do
-          expect(Version.create('1.2.3')).to be <= Version.create('v1.3')
-          expect(Version.create('1.2.3')).to be == Version.create('v1.2.3')
-          expect(Version.create('1.2.3')).to be >= Version.create('v1.1')
+          expect(v.create('1.2.3')).to be <= v.create('v1.3')
+          expect(v.create('1.2.3')).to be == v.create('v1.2.3')
+          expect(v.create('1.2.3')).to be >= v.create('v1.1')
         end
       end
 
       describe '#to_s' do
         it 'is converted into the original string' do
-          expect(Version.create('1.2.3').to_s).to be == '1.2.3'
-          expect(Version.create('v1.2.3').to_s).to be == 'v1.2.3'
+          expect(v.create('1.2.3').to_s).to be == '1.2.3'
+          expect(v.create('v1.2.3').to_s).to be == 'v1.2.3'
         end
       end
     end
