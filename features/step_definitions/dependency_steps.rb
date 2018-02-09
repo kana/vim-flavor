236a09487a1595993e2d666c4436e015425b5ed6
Kana Natsuno <dev@whileimautomaton.net>
2018-02-09 22:05:23 +0900

    Avoid referring to unexpected paths (2)

--- features/step_definitions/dependency_steps.rb
+++ features/step_definitions/dependency_steps.rb
@@ -1,5 +1,6 @@
 Then /^a dependency "(.*)" is stored in "(.*)"$/ do |repo_name, deps_path|
+  cache_path = "#{deps_path}/#{repo_name.zap}"
   steps %Q{
-    Then a directory named "#{deps_path}/#{repo_name.zap}" should exist
+    Then a directory named "#{path_for_step(cache_path)}" should exist
   }
 end
