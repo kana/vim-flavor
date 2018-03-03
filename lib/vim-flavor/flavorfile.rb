262dcb06084a70234850f783c5d3ff5b2e90e382
Kana Natsuno <dev@whileimautomaton.net>
2018-03-03 15:34:03 +0900

    Stop warning on loading flavorfile

--- lib/vim-flavor/flavorfile.rb
+++ lib/vim-flavor/flavorfile.rb
@@ -17,36 +17,14 @@ module Vim
       end
 
       def self.load_or_new(flavorfile_path)
-        _load_or_new(flavorfile_path, false)
+        ff = new()
+        ff.load(flavorfile_path) if File.exists?(flavorfile_path)
+        ff
       end
 
       def self.load(flavorfile_path)
-        _load_or_new(flavorfile_path, true)
-      end
-
-      private_class_method def self._load_or_new(flavorfile_path, must_exist)
-        # Note that flavorfile_path is assumed to be new name.
-        new_path = Pathname.new(flavorfile_path).relative_path_from(Pathname.getwd())
-        old_path = new_path.dirname() / 'VimFlavor'
-
         ff = new()
-
-        if FileTest.file?(new_path)
-          if FileTest.file?(old_path)
-            Console::warn "Delete #{old_path}.  #{new_path} is being read instead."
-          end
-          ff.load(new_path.to_s)
-        elsif FileTest.file?(old_path)
-          Console::warn "Rename #{old_path} to #{new_path}.  #{old_path} wll be ignored in future version."
-          ff.load(old_path.to_s)
-        else
-          if must_exist
-            throw "#{new_path} must be created to use vim-flavor."
-          else
-            # Do nothing.  Assume no dependencies.
-          end
-        end
-
+        ff.load(flavorfile_path)
         ff
       end
 
