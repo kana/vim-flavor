1bb16dffb97bac93614ab32893021c88dc3c8c37
Kana Natsuno <dev@whileimautomaton.net>
2018-03-03 15:32:24 +0900

    Define FlavorFile::path_from

--- lib/vim-flavor/flavorfile.rb
+++ lib/vim-flavor/flavorfile.rb
@@ -50,6 +50,27 @@ module Vim
         ff
       end
 
+      def self.path_from(dir_path, warn)
+        flavorfile_path = dir_path.to_flavorfile_path
+        new_path = Pathname.new(flavorfile_path).relative_path_from(Pathname.getwd())
+        old_path = new_path.dirname() / 'VimFlavor'
+
+        path = if FileTest.file?(new_path)
+          if warn and FileTest.file?(old_path)
+            Console::warn "Delete #{old_path}.  #{new_path} is being read instead."
+          end
+          new_path
+        elsif FileTest.file?(old_path)
+          if warn
+            Console::warn "Rename #{old_path} to #{new_path}.  #{old_path} wll be ignored in future version."
+          end
+          old_path
+        else
+          new_path
+        end
+        path.to_s
+      end
+
       def load(flavorfile_path)
         instance_eval(
           File.open(flavorfile_path, 'r').read(),
