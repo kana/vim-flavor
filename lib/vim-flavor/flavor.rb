df3f05c77e272410d61c31ab6cad60d623efa259
Kana Natsuno <dev@whileimautomaton.net>
2024-07-13 13:30:37 +0900

    Fix "E749: Empty buffer" on and after Vim 9.1.0573
    vim -c '
      some command
      another command
    '
    
    actually consists of 3 commands, not 2.
    The first command is "" which means the current line.
    This command effectively does nothing before Vim 9.1.0573,
    but it prints the current line since Vim 9.1.0573.
    Tt causes E749 because the current buffer is empty in this context.

--- lib/vim-flavor/flavor.rb
+++ lib/vim-flavor/flavor.rb
@@ -96,10 +96,10 @@ module Vim
             git checkout -f '#{locked_version.to_revision}' &&
             git checkout-index -a -f --prefix='#{deployment_path}/' &&
             {
-              vim -u NONE -i NONE -n -N -e -s -c '
-                silent! helptags #{deployment_path}/doc
-                qall!
-              ' || true
+              vim -u NONE -i NONE -n -N -e -s \
+                -c 'silent! helptags #{deployment_path}/doc' \
+                -c 'qall!' \
+                || true
             }
           } 2>&1
         ]
